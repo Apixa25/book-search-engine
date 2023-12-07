69d495f3169bb3f7f1eb129955df13cb53a5c9c1
stevensillsii <stevensills2@gmail.com>
2023-12-06 20:12:05 -0800

    auth.js updating the auth middleware function to work with the Graphql API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,10 +3,18 @@ const jwt = require('jsonwebtoken');
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
+// Update the auth middleware function to work with the GraphQL API.
+// The authMiddleware() function will execute with every request to the API.
+// If the user is authenticated, the authMiddleware() function will simply
+// move on and call the next() function. If the token cannot be verified,
+// the function will return an error.
+// The authMiddleware() function will be added as a second argument to the
+// ApolloServer constructor function in server.js.
+
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware() {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
