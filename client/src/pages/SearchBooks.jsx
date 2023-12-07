797791c6864874609969a67ce4fe2f0dfd9a0d5a
stevensillsii <stevensills2@gmail.com>
2023-12-06 21:10:44 -0800

    searchBooks.jsx modified to useMutation() Hook to execute the SAVE_BOOK function

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,8 +9,13 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import { useMutation } from '@apollo/react-hooks';
+import { SAVE_BOOK } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -20,6 +25,25 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  // create saveBook mutation
+  const [saveBook, { error }] = useMutation(SAVE_BOOK, {
+    update(cache, { data: { saveBook } }) {
+      try {
+        // read what's currently in the cache
+        const { me } = cache.readQuery({ query: GET_ME });
+        // prepend the newest saved book to the front of the array
+        cache.writeQuery({
+          query: GET_ME,
+          data: { me: { ...me, savedBooks: [...me.savedBooks, saveBook] } }
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+
+
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
