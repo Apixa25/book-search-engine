b21192275b44949e78ad076994c4f9d4bd1fc669
stevensillsii <stevensills2@gmail.com>
2023-12-08 15:03:33 -0800

    changes made inside an if satement around line 35 of the server.js

--- server/server.js
+++ server/server.js
@@ -29,8 +29,13 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+} 
+
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
